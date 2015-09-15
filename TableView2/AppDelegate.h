755cc370a24562ab71f1671b1532a2e035a28788
Bhavik Panchal <Bhavik@bhaviks-macbook-pro.local>
2015-09-15 17:09:21 +0530

    Initial Commit

---
+++ TableView2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableView2
+//
+//  Created by Bhavik Panchal on 9/15/15.
+//  Copyright © 2015 Bhavik Panchal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TableView2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableView2
+//
+//  Created by Bhavik Panchal on 9/15/15.
+//  Copyright © 2015 Bhavik Panchal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
